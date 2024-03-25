71f8eec69a7c6e787be9f568836a2f3b0613e4f5
djlee <moovinzoo@gmail.com>
2024-03-26 00:10:43 +0900

    feat: custom aliases

---
+++ aliases/custom.aliases.bash
@@ -0,0 +1,32 @@
+# shellcheck shell=bash
+about-alias 'custom aliases'
+
+alias ssh='kitty +kitten ssh'
+alias power='cat /sys/class/power_supply/BAT0/capacity'
+alias battery='power'
+alias vimrc='vim ~/.vimrc'
+alias kittyrc='vim ~/.config/kitty/kitty.conf'
+alias hyprrc='vim ~/.config/hypr/hyprland.conf'
+alias gitconfig='vim ~/.gitconfig'
+
+#alias l='LANG=C.UTF-8 ls -A --group-directories-first --indicator-style=classify --escape'
+alias l='exa -A --group-directories-first --git --icons --color=always'
+#alias ll='LANG=C.UTF-8 ls -lA --group-directories-first --indicator-style=classify --escape'
+alias ll='exa -lA --group-directories-first --git --icons --color=always'
+alias lt='exa -A --tree --git --color=always'
+
+# git
+alias ga='git add -A'
+alias gcb='git checkout -b'
+alias gcm='git commit -m'
+alias gd='git diff'
+alias gds='git diff --staged'
+alias gcb='git checkout -b'
+alias gst='git status'
+alias glo='git log --oneline'
+alias lazy='lazygit'
+
+#alias diff='git diff | delta --navigate --paging always'
+#alias diffs='git diff --staged | delta --navigate --paging always'
+#alias show='git show | delta --navigate --paging always'
+#alias log='git log --oneline'
